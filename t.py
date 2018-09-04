c0e335b0958e0f67a471b99a3d7caf16ce9381df
anjalisingh09 <38291012+anjalisingh09@users.noreply.github.com>
2018-09-05 00:28:01 +0530

    Update t.py

--- t.py
+++ t.py
@@ -2,13 +2,13 @@ from twilio.rest import Client
 
 
 # Your Account Sid and Auth Token from twilio.com/console
-account_sid = 'AC1eaf61ebe377cc0ce330980ac4b3c1a2'
-auth_token = '57d76d3d15a4ecf5b90a8f00e49eefe8'
+account_sid = '*******************************'
+auth_token = '********************************'
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(
                         url='http://demo.twilio.com/docs/voice.xml',
-                        to='+918727848306',                        				from_='+18125754021'
+                        to='***********',                        				from_='**********'
                     )
 
 print(call.sid)
